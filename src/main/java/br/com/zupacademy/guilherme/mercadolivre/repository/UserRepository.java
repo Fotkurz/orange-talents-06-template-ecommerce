8cd98e0cd6e594aeda83b548b7f1a8516f4b5143
Fotkurz <fotkurz@gmail.com>
2021-07-07 11:46:42 -0300

    Revert commit Cadastro usuario
    Refazendo os releases.

--- src/main/java/br/com/zupacademy/guilherme/mercadolivre/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.zupacademy.guilherme.mercadolivre.repository;
-
-import br.com.zupacademy.guilherme.mercadolivre.domain.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
